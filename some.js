5fb4934160070a801235c9ae19d7b81da71ea17a
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-09 19:53:36 -0400

    Create some function for challenge

--- some.js
+++ some.js
@@ -1,13 +1,21 @@
 /**
 Implement a some() function which takes an array of items and returns true if any item in the array is true
  */
-function some (items) {
-    return '';
+function some(items) {
+    let result = false
+    for (let i = 0; i < items.length; i++) {
+        if (items.includes(true)) {
+            result = true
+        }
+        return result
+    }
 }
 
 
 
- const result1 = some([false, false, false, false])
+
+const result1 = some([false, false, false, false])
+const array1 = [false, false, false, false]
 
 /*
  Learn something new!
@@ -16,7 +24,12 @@ function some (items) {
 
  https://developer.mozilla.org/en-US/docs/Web/API/console/assert
 */
- console.assert(result1 === false, "Expected false")
+console.assert(result1 === false, "Expected false")
 
- const result2 = some([false, false, false, true])
- console.assert(result2 === true, "Expected true")
+const result2 = some([false, false, false, true])
+const array2 = [false, false, false, true]
+
+console.assert(result2 === true, "Expected true")
+
+console.log(some(array1))
+console.log(some(array2))
