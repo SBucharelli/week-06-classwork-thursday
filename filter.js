e9db724eacc7dca003c25776ebeb2733d98dd3b8
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-09 21:02:57 -0400

    Create filter function

--- filter.js
+++ filter.js
@@ -2,10 +2,29 @@
 Implement a filter() function. 
 This function should take an array of items and a predicate function. 
 A predicate function is one that returns true or false. 
-The filter() function should return a new array that contains only the items from the original array for which the predicate function returns true. 
+The filter() function should return a new array that contains only the items from the original array for 
+which the predicate function returns true. 
  */
-function filter (items, predicate) {
-    return [];
+
+function filter(items, predicate) {
+    resultArray = []
+    for (let i = 0; i < items.length; i++) {
+        if ((predicate(items[i]))) {
+            resultArray.push(items[i])
+        }
+    }
+    return resultArray
+}
+
+//forEach version for reference of forEach loops
+function filter(items, predicate) {
+    resultArray = []
+    items.forEach(item => {
+        if (predicate(item)) {
+            resultArray.push(item)
+        }
+    });
+    return resultArray
 }
 
 const ages = [12, 14, 19, 20, 21, 22, 25, 30, 40];
@@ -13,6 +32,6 @@ const result1 = filter(ages, function (age) {
     return age >= 21
 })
 
-console.log({result1})
+console.log({ result1 })
 
 console.assert(result1.length === 5, "Expected 5 of the ages are legal drinking ages")
